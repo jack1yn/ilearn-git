aca1ef68ebfed384b0f69e75491b4851de540645
jack1yn <73976767+jack1yn@users.noreply.github.com>
2020-11-11 17:09:44 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = http.createServer((req, res) => {
 */
 
 app.get("/", (req, res) => {
-  res.send(["Hello from ilearn-git!"]);
+  res.send(["Hello Testing CI from ilearn-git - remote repo!"]);
  });
  
 app.get("/api", (req, res, next) => {
